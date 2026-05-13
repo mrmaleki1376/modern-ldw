0ea2c18a19373d32c536bf48abcde61abdb29da4
amirhossein <amirhossein.mohammadi.dev@gmail.com>
2026-05-13 14:47:00 +0330

    feat: add majeNoiseParams.m, update(addNoise.m): add extra comments

--- addNoise.m
+++ addNoise.m
@@ -1,15 +1,27 @@
-% This function is used to add different types of noise to a frame %
 
 function noisyFrame = addNoise(frame, noiseType, params)
+    % This function is used to add different types of noise to a frame
+    
+    % Inputs:
+    %   frame - Image or a video frame
+    %   noiseType - withoutnoise, gaussian, saltpepper or speckle
+    %   params - noiseParams.mat file containing mean, variance and density
+    %   params can be created using makeNoiseParams.m fucntion
 
+    % Outputs:
+    %   noisyFrame - A frame with noise applied to it
+
+    % Handle different number of input arguments
     if nargin < 3
         params = struct();
     end
 
+    % Type handling
     if ~isa(frame, 'uint8')
         frame = im2uint8(frame);
     end
 
+    % swtich case for different noise types
     switch lower(noiseType)
         case 'withoutnoise'
             noisyFrame = frame;
@@ -29,12 +41,12 @@ function noisyFrame = addNoise(frame, noiseType, params)
 end
 
 %{
+Usage Example:
 
 videoReader = VideoReader("video_assets\processed\highwayDashedLeftLine.mp4");
 
-noiseParams.mean = 0;
-noiseParams.variance = 0.01;
-noiseParams.density = 0.2;
+makeNoiseParams('noiseParams.mat', 0, 0.01, 0.1);
+noiseParams = load('noiseParams.mat').noiseParams;
 
 figure;
 
